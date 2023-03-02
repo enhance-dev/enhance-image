5063a573e5a3fe844d8e8786244e7f7998c432f2
Simon MacDonald <simon.macdonald@gmail.com>
2023-03-02 15:45:34 -0500

    Focalpoint (#4)
    * Update deps
    
    Signed-off-by: macdonst <simon.macdonald@gmail.com>
    
    * Enable focal point
    
    Signed-off-by: macdonst <simon.macdonald@gmail.com>
    
    * Update README
    
    Signed-off-by: macdonst <simon.macdonald@gmail.com>
    
    ---------
    
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ export const getVariants = (attrs) => Object
   .filter(key => key.startsWith('variant'))
   .reduce((variants, currentVariant) => {
     // Split the current variant attribute's value on the space following the closing media query parenthesis
-    const [media, width] = attrs[currentVariant].split(/(?<=\))\s/)
-    return [...variants, { media: media.trim(), width: width.trim() }]
+    const [media, dimensions] = attrs[currentVariant].split(/(?<=\))\s/)
+    const [width, height = ""] = dimensions.split(/\s/)
+    return [...variants, { media: media.trim(), width: width.trim(), height: height.trim() }]
   }, [])
+
+function getXY(focalpoint = "50,50") {
+  return focalpoint.split(',')
+}
 
 export default function EnhanceImage({ html, state }) {
   const { attrs } = state
-  const { alt, defaultwidth, src, loading, ...otherAttrs } = attrs
+  const { alt, defaultwidth, src, loading, focalpoint, mark, ...otherAttrs } = attrs
 
+  const [x, y] = getXY(focalpoint)
   const variants = getVariants(otherAttrs)
-  const getOptimizedSrc = width => `/transform${src}?width=${width}`
-  const variantsMarkup = variants.map(variant => `<source media='${variant.media}' srcset='${getOptimizedSrc(variant.width)}' />`).join('')
+  const getOptimizedSrc = (width, height) => `/transform${src}?width=${width}${focalpoint && height ? `&height=${height}&fit=cover&focus=point&x=${x}&y=${y}${mark !== undefined ? `&mark` : ''}` : ''}`
+  const variantsMarkup = variants.map(variant => `<source media='${variant.media}' srcset='${getOptimizedSrc(variant.width, variant.height)}' />`).join('')
   const loadingAttr = loading ? `loading='${loading}'` : ''
-  
+
   return html`
     <picture>
       ${variantsMarkup}
