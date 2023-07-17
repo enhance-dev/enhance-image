e67059ec597f03693930b0227ff43d883a743f9a
Cole Peters <cole@colepeters.com>
2023-07-17 12:58:40 -0500

    Initial v1:
    - Reexport arc-image-plugin as enhance-image-plugin to reduce confusion
    - Write new component
    - Expose plugin via default export and component via named export

---
+++ enhance-image.mjs
@@ -0,0 +1,44 @@
+import { formatPath } from '@enhance/arc-image-plugin'
+import config from '@architect/shared/enhance-image/image-config.mjs'
+
+export default function EnhanceImage({ html, state }) {
+  const { attrs } = state
+  const {
+    alt = '',
+    src,
+    sizes = '100vw',
+    height,
+    width,
+  } = attrs
+
+  const loadingStrategy = Object.keys(attrs).includes('priority') ? 'fetchpriority="high"' : 'loading="lazy"'
+
+  // Generate a srcset for the source image using the image config
+  const { widths, format, quality } = config
+  const srcset = widths.map(width => {
+    return `${formatPath({
+      src,
+      width,
+      quality,
+      format,
+    })} ${width}w`
+  }).join(', ')
+
+  return html`
+    <style>
+      img {
+        max-inline-size: 100%;
+        block-size: auto;
+      }
+    </style>
+    <img
+      alt='${alt}'
+      srcset='${srcset}'
+      sizes='${sizes}'
+      width='${width}'
+      height='${height}'
+      ${loadingStrategy}
+    />
+  `
+}
+
--- index.js
+++ index.js
@@ -1,48 +1,7 @@
-export const getVariants = (attrs) => Object
-  .keys(attrs)
-  .filter(key => key.startsWith('variant'))
-  .reduce((variants, currentVariant) => {
-    // Split the current variant attribute's value on the space following the closing media query parenthesis
-    const [media, dimensions] = attrs[currentVariant].split(/(?<=\))\s/)
-    const [width, height = ""] = dimensions.split(/\s/)
-    return [...variants, { media: media.trim(), width: width.trim(), height: height.trim() }]
-  }, [])
+const plugin = require('@enhance/arc-image-plugin')
+const EnhanceImage = require('./enhance-image.mjs')
 
-function getXY(focalpoint = "50,50") {
-  return focalpoint.split(',')
-}
-
-function paramString({width, height, focalpoint, x, y, mark}) {
-  const params = [`width_${width}`]
-  if (focalpoint && height) {
-    params.push(`height_${height}`)
-    params.push(`fit_cover`)
-    params.push(`focus_point`)
-    params.push(`x_${x}`)
-    params.push(`y_${y}`)
-    if (mark !== undefined) {
-      params.push('mark_true')
-    }
-  }
-  return params.join(`,`)
-}
-
-export default function EnhanceImage({ html, state }) {
-  const { attrs } = state
-  const { alt, defaultwidth, src, loading, focalpoint, mark, ...otherAttrs } = attrs
-
-  const [x, y] = getXY(focalpoint)
-  const variants = getVariants(otherAttrs)
-  const getOptimizedSrc = (width, height) => `/transform/${paramString({width, height, focalpoint, x, y, mark})}${src}`
-  const variantsMarkup = variants.map(variant => `<source media='${variant.media}' srcset='${getOptimizedSrc(variant.width, variant.height)}' />`).join('')
-  const loadingAttr = loading ? `loading='${loading}'` : ''
-
-  return html`
-    <picture>
-      ${variantsMarkup}
-      <source srcset='${getOptimizedSrc(defaultwidth)}' />
-      <img src='${src}' alt='${alt}' ${loadingAttr} />
-    </picture>
-  `
-}
+module.exports = plugin // export for arc plugin interface
+exports = module.exports // ESM default export
+exports.EnhanceImage = EnhanceImage // ESM named export for component
 
